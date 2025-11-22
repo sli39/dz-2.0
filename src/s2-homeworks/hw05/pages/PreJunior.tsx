68e060303eab076b8fffb55b109f1b1bee7b5247
sli39 <sliva391985@gmail.com>
2025-11-22 17:30:50 +0300

    Fix ESLint warnings: use HW3 and HW4

--- src/s2-homeworks/hw05/pages/PreJunior.tsx
+++ src/s2-homeworks/hw05/pages/PreJunior.tsx
@@ -10,8 +10,8 @@ function PreJunior() {
             {/*pre junior page*/}
             <HW1 />
             <HW2 />
-            {/*<HW3 />*/}
-            {/*<HW4 />*/}
+            <HW3 />
+            <HW4 />
         </div>
     )
 }
